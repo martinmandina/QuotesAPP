798e9cf16bbcf2bfafaf1fc99a7894c80c991c6e
martinsmandina <martinsmandina@gmail.com>
2020-09-26 19:09:53 +0300

    change and add quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,12 +10,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1, name:'moja',message:'Watch finding Nemoo'},
-    {id:2,name:'mbili',message:'Buy Cookies'},
-    {id:3,name:'tatu',message:'Get new Phone Case'},
-    {id:4,name:'nne',message:'Get Dog Food'},
-    {id:5,name:'tano',message:'Solve math homework'},
-    {id:6,name:'sita',message:'dfgjhfgj'},
+    {id:1, name:'Albert Einstein',message:'Two things are infinite the universe and human stupidity; and Im not sure about the universe'},
+    {id:2,name:'Mae West',message:'You only live once, but if you do it right, once is enough'},
+    {id:3,name:'Mahatma Gandhi',message:'Be the change that you wish to see in the world'},
+    {id:4,name:'Eleanor Roosevelt',message:'No one can make you feel inferior without your consent'},
+    {id:5,name:'Mark Twain',message:'If you tell the truth, you dont have to remember anything'},
+
   ];
 
   constructor() { }
